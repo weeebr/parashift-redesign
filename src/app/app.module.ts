1c36a8e386070172d0294dbbee148f47c35444bb
Pascal Weber <pascal.weber@parashift.io>
2021-07-23 12:45:35 +0200

    add actions on hover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { HeaderComponent } from './page-layout/header/header.component';
 import { TabsComponent } from './page-layout/tabs/tabs.component';
+import { NavComponent } from './page-layout/nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TabsComponent } from './page-layout/tabs/tabs.component';
     ListComponent,
     HeaderComponent,
     TabsComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
